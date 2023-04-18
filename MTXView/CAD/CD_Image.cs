36eac9b9bc0437462803033e2cce6473c2a78082
Mael Machuca <100020513+MiniMahe@users.noreply.github.com>
2023-04-18 21:04:34 +0200

    Cambiar nombres (error mio)

--- MTXView/CAD/CD_Image.cs
+++ MTXView/CAD/CD_Image.cs
@@ -1,51 +1,13 @@
-﻿using CapaDatos;
-using Microsoft.Identity.Client;
-using MySql.Data.MySqlClient;
-using System.Data;
-using System.Data.SqlClient;
-using System.Net.Http;
-using System.Security.Claims;
+﻿
 
-namespace Datos
+namespace Image
 {
-    public class CD_User {
-        MySqlDataReader leer;
-        
-        public string Verify(string nombre, string password)
+    public class Img
+    {
+        public string LoadImage()
         {
-            try
-            {
-                using (MySqlConnection conexionsql = new(CD_Conexion.ConexionStr())) { 
-                    using (MySqlCommand comando = new MySqlCommand("Verificar", conexionsql))
-                    {
-                        comando.CommandType = CommandType.StoredProcedure;
-                        comando.Parameters.Add("name", MySqlDbType.VarChar).Value = nombre;
-                        comando.Parameters.Add("pass", MySqlDbType.VarChar).Value = password;
-                        conexionsql.Open();
+            return null;
 
-                        leer = comando.ExecuteReader();
-
-                        if (leer.Read())
-                        {
-                            conexionsql.Close();
-                            return nombre;
-                        }
-                        else
-                        {
-                            conexionsql.Close();
-
-                            return null;
-                        }
-
-
-                    }
-                }
-            }
-            catch (Exception)
-            {
-
-                return null;
-            }
-            }
         }
     }
+}
--- MTXView/CAD/CD_User.cs
+++ MTXView/CAD/CD_User.cs
@@ -6,14 +6,46 @@ using System.Data.SqlClient;
 using System.Net.Http;
 using System.Security.Claims;
 
-namespace Image
+namespace Datos
 {
-    public class Img
-    {
-        public string LoadImage()
+    public class CD_User {
+        MySqlDataReader leer;
+        
+        public string Verify(string nombre, string password)
         {
-            return null;
+            try
+            {
+                using (MySqlConnection conexionsql = new(CD_Conexion.ConexionStr())) { 
+                    using (MySqlCommand comando = new MySqlCommand("Verificar", conexionsql))
+                    {
+                        comando.CommandType = CommandType.StoredProcedure;
+                        comando.Parameters.Add("name", MySqlDbType.VarChar).Value = nombre;
+                        comando.Parameters.Add("pass", MySqlDbType.VarChar).Value = password;
+                        conexionsql.Open();
 
+                        leer = comando.ExecuteReader();
+
+                        if (leer.Read())
+                        {
+                            conexionsql.Close();
+                            return nombre;
+                        }
+                        else
+                        {
+                            conexionsql.Close();
+
+                            return null;
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+            }
         }
     }
-}
