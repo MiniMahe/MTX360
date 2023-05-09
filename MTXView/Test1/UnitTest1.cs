1e43f080533daedaa945d251ba1e4646d14f7df8
Mael Machuca <100020513+MiniMahe@users.noreply.github.com>
2023-05-09 21:10:37 +0200

    Añadir Tests

--- MTXView/Test1/UnitTest1.cs
+++ MTXView/Test1/UnitTest1.cs
@@ -1,6 +1,7 @@
 using MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using MVC.Controllers;
+using Microsoft.AspNetCore.Http;
 
 namespace Test1
 {
@@ -8,7 +9,7 @@ namespace Test1
 	public class UnitTest1
 	{
 		[TestMethod]
-		public void DaFlechasAlaView()
+		public void DaFlechasView()
 		{
 			var controller = new HomeController();
 			var result = controller.Flechas();
@@ -22,5 +23,28 @@ namespace Test1
 			var result = controller.Flechas() as ViewResult;
 			Assert.IsInstanceOfType(result.Model, typeof(Arrows));
 		}
-	}
+        [TestMethod]
+        public void LoginDevuelveLoginNoAutenticado()
+        {
+            var controller = new AccessController();
+            controller.ControllerContext = new ControllerContext();
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            var result = controller.Login() as ViewResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Login", result.ViewName);
+        }
+        [TestMethod]
+        public void Planta1DevuelveClases()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Planta1() as ViewResult;
+            var model = result.Model as ListaClases;
+
+            Assert.IsNotNull(model);
+            Assert.IsTrue(model.Salas.Count > 0);
+        }
+    }
 }
